a1847d3baaafa685f4935798ea9c36b1bd3d4cde
Gernot Glawe <gglawe@tecracer.de>
2021-02-19 17:21:12 +0100

    Ubuntu not needed

--- main.tf
+++ main.tf
@@ -3,21 +3,6 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
 
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
