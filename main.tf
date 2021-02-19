dffa31edb7e4836be6873ca951c785a0818de7e4
Gernot Glawe <gglawe@tecracer.de>
2021-02-19 12:18:21 +0100

    Jenkins up and running

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.web_traffic.name]
+  key_name = "labkey"
+  user_data = file("userdata/jenkins.sh")
+
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("labkey.pem")
+  }
+
+  tags = {
+    "Name"      = "Jenkins_Server"
+    "Terraform" = "true"
+  }
+}
---
+++ security.tf
@@ -0,0 +1,31 @@
+variable "ingressrules" {
+  type    = list(number)
+  default = [8080, 443, 22]
+}
+
+resource "aws_security_group" "web_traffic" {
+  name        = "Allow web traffic"
+  description = "Allow ssh and standard http/https ports inbound and everything outbound"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+}
