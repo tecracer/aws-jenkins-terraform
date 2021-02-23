8a1536a9a33e7aaace356942cca59230e85f5827
Gernot Glawe <gglawe@tecracer.de>
2021-02-23 08:25:51 +0100

    Userdata instead of ssh

--- main.tf
+++ main.tf
@@ -17,16 +17,9 @@ resource "aws_instance" "jenkins" {
   ami = data.aws_ami.amazon-linux-2.id
   instance_type = "t3.micro"
   security_groups = [aws_security_group.web_traffic.name]
-  key_name = "labkey"
   user_data = file("userdata/jenkins.sh")
   iam_instance_profile = "JenkinsProfile"
 
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("labkey.pem")
-  }
 
   tags = {
     "Name"      = "Jenkins_Server"
